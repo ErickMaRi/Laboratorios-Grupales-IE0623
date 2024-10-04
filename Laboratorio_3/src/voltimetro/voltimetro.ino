bc35b60005b13ca592d92f5365c2c26f4f0ebcaf
christabel-alvarado <christabel.alvarado@gmail.com>
2024-10-04 09:30:55 -0600

    4 voltimetros integrados

--- Laboratorio_3/src/voltimetro/voltimetro.ino
+++ Laboratorio_3/src/voltimetro/voltimetro.ino
@@ -8,24 +8,31 @@ void setup(){
   //Configurar los pines como salida
   Serial.begin(9600);
   pinMode(2,OUTPUT);
+  pinMode(3,OUTPUT);
+  pinMode(4,OUTPUT);
+  pinMode(5,OUTPUT);
   pinMode(A4, INPUT);
   pinMode(A0, INPUT);
+  pinMode(A1, INPUT);
+  pinMode(A2, INPUT);
+  pinMode(A3, INPUT);
 
 }
-void alarma(float v, int tipo){
+void alarma(float v, int tipo, int pin){
+  //Encender Leds cuando se mida +-20V
   if (tipo == 0){ //AC
-    if(v > 14.14) digitalWrite(2, HIGH); // encender LED
-    else digitalWrite(2, LOW);
+    if(v > 14.14) digitalWrite(pin, HIGH); // encender LED
+    else digitalWrite(pin, LOW);
   }else if (tipo == 1){//DC
-    if( v > 20 || v < -20) digitalWrite(2, HIGH);  // encender LED
-    else digitalWrite(2, LOW);
+    if( v > 20 || v < -20) digitalWrite(pin, HIGH);  // encender LED
+    else digitalWrite(pin, LOW);
   }
 }
 
-float calcular_rms() {
+float calcular_rms(int pin) {
   v_ac_pico = 0.00;
   for(int i = 0; i < 100; i++) {
-    v_ac = analogRead(A0); 
+    v_ac = analogRead(pin); 
     v_ac = 9.59743 * (v_ac * 5 / 1023) -23.99999; 
     Serial.print("Voltage adjusted: ");
     Serial.println(v_ac);
@@ -35,34 +42,72 @@ float calcular_rms() {
 
     Serial.print("Voltage max: ");
     Serial.println(v_ac_pico);
-  v_ac = v_ac_pico/sqrt(2); //Calcular RMS
-  return v_ac;
+    v_ac = v_ac_pico/sqrt(2); //Calcular RMS
+    return v_ac;
 }
 
+float ajustar_dc(int pin){
+  v = analogRead(pin);
+  v = 9.59743 * (v * 5 / 1023) -23.99999;
+}
 
 void loop(){
-  Serial.print("Loop\n");
   if (analogRead(A4) < 512){ // Si switch cerrado medir AC
  
     //Leer y ajustar voltaje   
-    v = calcular_rms();
-    alarma(v, 0);
-
+    v = calcular_rms(A0);
+    alarma(v, 0, 2);
     //Pruebas
-    Serial.print("Voltage AC: ");
+    Serial.print("Voltage 1 AC : ");
+    Serial.println(v);
+    delay(1000);
+    //Leer y ajustar voltaje   
+    v = calcular_rms(A1);
+    alarma(v, 0, 3);
+    //Pruebas
+    Serial.print("Voltage 1 AC : ");
+    Serial.println(v);
+    delay(1000);
+    //Leer y ajustar voltaje   
+    v = calcular_rms(A2);
+    alarma(v, 0, 4);
+    //Pruebas
+    Serial.print("Voltage 1 AC : ");
+    Serial.println(v);
+    delay(1000);
+    //Leer y ajustar voltaje   
+    v = calcular_rms(A3);
+    alarma(v, 0, 5);
+    //Pruebas
+    Serial.print("Voltage 1 AC : ");
     Serial.println(v);
     delay(1000);
 
   } 
   else{ // si switch abierto medir DC
 
-    //Leer y ajustar voltaje
-    v = analogRead(A0);
-    v = 9.59743 * (v * 5 / 1023) -23.99999;
-    alarma(v, 1);
-
+    ajustar_dc(A0)
+    alarma(v, 1, 2);
     //Pruebas
-    Serial.print("Voltage DC: ");
+    Serial.print("Voltage 1 DC: ");
+    Serial.println(v);
+    delay(1000);
+    ajustar_dc(A1)
+    alarma(v, 1, 3);
+    //Pruebas
+    Serial.print("Voltage 2 DC: ");
+    Serial.println(v);
+    delay(1000);
+    ajustar_dc(A2)
+    alarma(v, 1, 4);
+    //Pruebas
+    Serial.print("Voltage  3 DC: ");
+    Serial.println(v);
+    delay(1000);
+    ajustar_dc(A3)
+    alarma(v, 1, 5);
+    //Pruebas
+    Serial.print("Voltage 4 DC: ");
     Serial.println(v);
     delay(1000);
   }
