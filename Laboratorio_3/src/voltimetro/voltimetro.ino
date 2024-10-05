2b04ad480e995b126f95f5eaabf31d332b64032c
christabel-alvarado <christabel.alvarado@gmail.com>
2024-10-05 15:00:16 -0600

    Correccion de variables

--- Laboratorio_3/src/voltimetro/voltimetro.ino
+++ Laboratorio_3/src/voltimetro/voltimetro.ino
@@ -1,6 +1,7 @@
 
 //Definicion de variables globales
 float v = 0.00;
+float v_a0, v_a1, v_a2, v_a3 = 0.00;
 float v_ac_pico = 0.00;
 float v_ac = 0.00;
 
@@ -44,36 +45,37 @@ float calcular_rms(int pin) {
 float ajustar_dc(int pin){
   v = analogRead(pin);
   v = 9.59743 * (v * 5 / 1023) -23.99999;
+  return v;
 }
 
 void loop(){
   if (analogRead(A4) < 512){ // Si switch cerrado medir AC
     
     //Voltímetro 1   
-    v = calcular_rms(A0);
-    alarma(v, 0, 2);
+    v_a0 = calcular_rms(A0);
+    alarma(v_a0, 0, 2);
     //Voltímetro 2 
-    v = calcular_rms(A1);
-    alarma(v, 0, 3);
+    v_a1 = calcular_rms(A1);
+    alarma(v_a1, 0, 3);
     //Voltímetro 3
-    v = calcular_rms(A2);
-    alarma(v, 0, 4);
+    v_a2 = calcular_rms(A2);
+    alarma(v_a2, 0, 4);
     //Voltímetro 4   
-    v = calcular_rms(A3);
-    alarma(v, 0, 5);
+    v_a3 = calcular_rms(A3);
+    alarma(v_a3, 0, 5);
   } 
   else{ // si switch abierto medir DC
     //Voltímetro 1
-    ajustar_dc(A0);
-    alarma(v, 1, 2);
+    v_a0 = ajustar_dc(A0);
+    alarma(v_a0, 1, 2);
     //Voltímetro 2
-    ajustar_dc(A1);
-    alarma(v, 1, 3);
+    v_a1 = ajustar_dc(A1);
+    alarma(v_a1, 1, 3);
     //Voltímetro 3
-    ajustar_dc(A2);
-    alarma(v, 1, 4);
+    v_a2 = ajustar_dc(A2);
+    alarma(v_a2, 1, 4);
     //Voltímetro 4
-    ajustar_dc(A3);
-    alarma(v, 1, 5);
+    v_a3 = ajustar_dc(A3);
+    alarma(v_a3, 1, 5);
   }
 }
