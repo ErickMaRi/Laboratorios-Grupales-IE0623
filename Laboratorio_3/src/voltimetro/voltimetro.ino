ca3cbba52634e9ddc8ea37cb8f1e2317463f8b17
christabel-alvarado <christabel.alvarado@gmail.com>
2024-10-05 03:51:31 -0600

    Limpiar código, eliminar pruebas

--- Laboratorio_3/src/voltimetro/voltimetro.ino
+++ Laboratorio_3/src/voltimetro/voltimetro.ino
@@ -34,14 +34,9 @@ float calcular_rms(int pin) {
   for(int i = 0; i < 100; i++) {
     v_ac = analogRead(pin); 
     v_ac = 9.59743 * (v_ac * 5 / 1023) -23.99999; 
-    Serial.print("Voltage adjusted: ");
-    Serial.println(v_ac);
     if(v_ac > v_ac_pico) v_ac_pico = v_ac;
     delayMicroseconds(100);
   }
-
-    Serial.print("Voltage max: ");
-    Serial.println(v_ac_pico);
     v_ac = v_ac_pico/sqrt(2); //Calcular RMS
     return v_ac;
 }
@@ -53,62 +48,32 @@ float ajustar_dc(int pin){
 
 void loop(){
   if (analogRead(A4) < 512){ // Si switch cerrado medir AC
- 
-    //Leer y ajustar voltaje   
+    
+    //Voltímetro 1   
     v = calcular_rms(A0);
     alarma(v, 0, 2);
-    //Pruebas
-    Serial.print("Voltage 1 AC : ");
-    Serial.println(v);
-    delay(1000);
-    //Leer y ajustar voltaje   
+    //Voltímetro 2 
     v = calcular_rms(A1);
     alarma(v, 0, 3);
-    //Pruebas
-    Serial.print("Voltage 1 AC : ");
-    Serial.println(v);
-    delay(1000);
-    //Leer y ajustar voltaje   
+    //Voltímetro 3
     v = calcular_rms(A2);
     alarma(v, 0, 4);
-    //Pruebas
-    Serial.print("Voltage 1 AC : ");
-    Serial.println(v);
-    delay(1000);
-    //Leer y ajustar voltaje   
+    //Voltímetro 4   
     v = calcular_rms(A3);
     alarma(v, 0, 5);
-    //Pruebas
-    Serial.print("Voltage 1 AC : ");
-    Serial.println(v);
-    delay(1000);
-
   } 
   else{ // si switch abierto medir DC
-
-    ajustar_dc(A0)
+    //Voltímetro 1
+    ajustar_dc(A0);
     alarma(v, 1, 2);
-    //Pruebas
-    Serial.print("Voltage 1 DC: ");
-    Serial.println(v);
-    delay(1000);
-    ajustar_dc(A1)
+    //Voltímetro 2
+    ajustar_dc(A1);
     alarma(v, 1, 3);
-    //Pruebas
-    Serial.print("Voltage 2 DC: ");
-    Serial.println(v);
-    delay(1000);
-    ajustar_dc(A2)
+    //Voltímetro 3
+    ajustar_dc(A2);
     alarma(v, 1, 4);
-    //Pruebas
-    Serial.print("Voltage  3 DC: ");
-    Serial.println(v);
-    delay(1000);
-    ajustar_dc(A3)
+    //Voltímetro 4
+    ajustar_dc(A3);
     alarma(v, 1, 5);
-    //Pruebas
-    Serial.print("Voltage 4 DC: ");
-    Serial.println(v);
-    delay(1000);
   }
 }
