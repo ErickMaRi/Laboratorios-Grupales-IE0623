2c08b13cdc18704755a230c57328f7cc3562220b
ErickMaRi <erick.marinrojas@ucr.ac.cr>
2024-10-30 20:01:00 -0600

    Detalles finales

--- Laboratorio_4/src/enviar_iot.py
+++ Laboratorio_4/src/enviar_iot.py
@@ -3,15 +3,15 @@ import time
 import json
 import requests
 
-# Configuración del puerto serial (actualiza el puerto según corresponda)
+# Configuramos el puerto serial
 SERIAL_PORT = '/dev/ttyACM0'  
 BAUD_RATE = 115200  
 
-# Configuración de ThingsBoard
+# Configuramos los dominios y token de ThingsBoard
 THINGSBOARD_HOST = 'https://thingsboard.cloud'  
 ACCESS_TOKEN = 'elzMPhnOoIIf8rJ5u5ZA'  
 
-# Inicializar conexión serial
+# Inicializamos la conexión
 try:
     ser = serial.Serial(SERIAL_PORT, BAUD_RATE, timeout=1)
     print(f"Conectado al puerto serial {SERIAL_PORT}")
@@ -47,7 +47,7 @@ def main():
                             'gyro_x': float(datos[0]),
                             'gyro_y': float(datos[1]),
                             'gyro_z': float(datos[2]),
-                            'bateria': (50)*(float(datos[3])-7)
+                            'bateria': (50)*(float(datos[3])-7) 
                         }
                         enviar_datos_thingsboard(telemetria)
                     except ValueError as e:
