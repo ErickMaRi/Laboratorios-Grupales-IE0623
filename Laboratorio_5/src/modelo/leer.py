4a68151371b7ac72cfd26363aeda15e6cd774085
christabel-alvarado <christabel.alvarado@gmail.com>
2024-11-13 13:29:26 -0600

    Modelo entrenado

--- Laboratorio_5/src/modelo/leer.py
+++ Laboratorio_5/src/modelo/leer.py
@@ -5,10 +5,10 @@ import numpy as np
 
 def leer_datos(carpeta, index):
     # Set base path to Google Drive folder
-    ruta_base = '/content/drive/MyDrive/lab5micros/datos'  
+    ruta_base = '/content/drive/MyDrive/lab5micros/datos'
     ruta_carpeta = os.path.join(ruta_base, carpeta)
-    input = []  
-    output= [] 
+    input = []
+    output= []
 
     # Check if folder exists
     if not os.path.isdir(ruta_carpeta):
@@ -43,9 +43,21 @@ def preparar_datos():
         inputs.extend(matriz_datos)
         outputs.extend(identificador_archivos)
 
+
+
+    num_max = max([max([max([abs(float(data)) for data in point])] for point in input) for input in inputs])[0]
+    print("max accelaration:", num_max)
+    for i in range(len(inputs)):
+      for j in range(len(inputs[i])):
+        for k in range(len(inputs[i][j])):
+          inputs[i][j][k] = (float(inputs[i][j][k])+ num_max/2)/(2*num_max)
+
+    inputs = [[data for point in input for data in point] for input in inputs]
+    print(outputs)
+
     inputs = np.array(inputs, dtype=float)
     outputs = np.array(outputs, dtype=float)
-    
+
     # Random shuffle
     indices = np.arange(len(inputs))
     np.random.shuffle(indices)
@@ -57,33 +69,34 @@ def preparar_datos():
     num_validacion = int(0.2 * len(inputs))
 
     x_train, y_train = inputs[:num_entrenamiento], outputs[:num_entrenamiento]
-    x_val, y_val = inputs[num_entrenamiento:num_entrenamiento + num_validacion], outputs[num_entrenamiento:num_validacion + num_validacion]
+    x_val, y_val = inputs[num_entrenamiento:num_entrenamiento + num_validacion], outputs[num_entrenamiento:num_entrenamiento + num_validacion]
     x_test, y_test = inputs[num_entrenamiento + num_validacion:], outputs[num_entrenamiento + num_validacion:]
 
     return x_train, y_train, x_val, y_val, x_test, y_test
 
 def crear_modelo():
     modelo = tf.keras.models.Sequential([
-        tf.keras.layers.Dense(64, activation='relu', input_shape=(300,)),
+        tf.keras.layers.Dense(64, activation='relu'),
         tf.keras.layers.Dense(32, activation='relu'),
         tf.keras.layers.Dense(3, activation='softmax')  # Three outputs for the three types of movement
     ])
 
-    modelo.compile(optimizer='rmsprop',
-                   loss='mae',
-                   metrics=['accuracy'])
+    modelo.compile(optimizer='rmsprop', loss='mse', metrics=['mae'])
     return modelo
     
 def main():
     from re import X
     # Prepare data
-    input_train, output_train, input_val, output_val, input_test, output_test = preparar_datos()
 
-    # Uncomment the following lines if you want to train and save the model
-    # model = crear_modelo()
-    # model.fit(x_train, y_train, epochs=20, validation_data=(x_val, y_val))
-    # test_loss, test_acc = model.evaluate(x_test, y_test)
-    # print(f"Test accuracy: {test_acc}")
+    input_train, output_train, input_val, output_val, input_test, output_test = preparar_datos()
+    print(len(input_val))
+    print(len(output_val))
+
+    model = crear_modelo()
+    model.fit(input_train, output_train, epochs=100, validation_data=(input_val, output_val))
+    test_loss, test_acc = model.evaluate(input_test, output_test)
+    print(f"Test loss: {test_loss}")
+    print(f"Test accuracy: {test_acc}")
 
     # Convert model to TensorFlow Lite
     # converter = tf.lite.TFLiteConverter.from_keras_model(model)
